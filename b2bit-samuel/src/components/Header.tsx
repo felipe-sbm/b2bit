4b4e94add7a132f5ddefb933089dc783e1294737
Felipe Samuel <129466661+felipe-sbm@users.noreply.github.com>
2024-05-15 15:37:34 -0300

    Alterando logos

--- b2bit-samuel/src/components/Header.tsx
+++ b2bit-samuel/src/components/Header.tsx
@@ -4,13 +4,9 @@ import './global.css';
 function Header() {
     return (
         <header>
-            <img src="logo.png" alt="Company Logo" className="logo" />
             <nav>
                 <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/">Logout</a></li>
                 </ul>
             </nav>
         </header>
