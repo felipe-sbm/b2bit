eb82659348519bab7aa827d5168686a894f40010
felipe-sbm <felipesbm2005@gmail.com>
2024-05-20 22:13:07 -0300

    Enviando novamente a API

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -6,15 +6,15 @@ const api = axios.create({
 
 export const useApi = () => ({
   validateToken: async (token: string) => {
-    const response = await api.post('/validate', { token });
+    const response = await api.post('/validate/', { token });
     return response.data;
   },
   signin: async (email: string, password: string) => {
-    const response = await api.post('/login', { email, password });
+    const response = await api.post('/login/', { email, password });
     return response.data;
   },
   logout: async () => {
-    const response = await api.post('/logout');
+    const response = await api.post('/logout/');
     return response.data;
   },
 });
